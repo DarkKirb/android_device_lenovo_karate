8d69457023f571133296ff9a87c68622051501a9
karthick111 <karthickrko61@gmail.com>
2017-10-09 17:59:04 +0200

    karate: add karate specific camera configs

---
+++ product/qcom-camera.mk
@@ -0,0 +1,7 @@
+# Camera
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/camera/imx219_karateof_chromatix.xml:system/etc/camera/imx219_karateof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/imx219_karateqt_chromatix.xml:system/etc/camera/imx219_karateqt_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/imx258_karateof_chromatix.xml:system/etc/camera/imx258_karateof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/imx258_karateqt_chromatix.xml:system/etc/camera/imx258_karateqt_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/lenovo_karate_camera.xml:system/etc/camera/lenovo_karate_camera.xml
